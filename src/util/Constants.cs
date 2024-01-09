61e6da5dc809f78febb5b89325278703d0a280e7
Albert Michael Ybanez <412272+charizardcharz@users.noreply.github.com>
2024-01-09 10:56:23 -0500

    Increment version

--- src/util/Constants.cs
+++ src/util/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public const string APPLICATION_NAME = "Immich Go GUI";
-        public const string VERSION = "1.0.1";
+        public const string VERSION = "1.1";
 
         private Constants()
         {
